23b6a91529c91dc5fef7e867231773ecd4646f1f
ldevernay <boblehamster@yahoo.fr>
2018-02-07 11:18:05 +0100

    update icons

--- src/containers/project_detail.js
+++ src/containers/project_detail.js
@@ -14,6 +14,18 @@ class ProjectDetail extends Component {
             <div className="col-sm-8">
                 <div className="title">
                     {project.data.name}
+                    <div>
+                        <a href={project.data.html_url} role="button">
+                            <div className="github-icon">
+                                <i aria-hidden="true" className="fa fa-star fa-fw"></i>
+                                {project.data.stargazers_count}
+                                <i aria-hidden='true' className='fa fa-eye fa-fw' />
+                                {project.data.watchers_count}
+                                <i aria-hidden='true' className='fas fa-code-branch' />
+                                {project.data.forks_count}
+                            </div>
+                        </a>
+                    </div>
                 </div>
                 <div className="card-body info">
                     <p className="card-text">
@@ -27,35 +39,17 @@ class ProjectDetail extends Component {
 
                     <div className="row">
                         <div className="col-sm-5">
-                            <a href={project.data.html_url} className="btn repo">
-                                <i className="fab fa-github"></i>
+                            <a href={project.data.html_url} className="btn repo" title="Github repo">
+                                <i className="fab fa-github link-icon"></i>
                             </a>
                         </div>
                         <div className="col-sm-5 col-sm-offset-2">
-                            <a href={project.data.html_url} className="btn repo">
-                                Go to live demo
-                </a>
+                            <a href={project.data.html_url} className="btn repo" title="Live demo">
+                                <i className="fas fa-desktop link-icon"></i>
+                            </a>
                         </div>
                     </div>
                 </div>
-
-                <div className="project-status">
-                    <a href={project.data.html_url} role="button">
-                        <p title="Stars">
-                            <i aria-hidden="true" className="fa fa-star fa-fw"></i>
-                            {project.data.stargazers_count}
-                        </p>
-                        <p title='Watching'>
-                            <i aria-hidden='true' className='fa fa-eye fa-fw' />
-                            {project.data.watchers_count}
-                        </p>
-                        <p title='Forks'>
-                            <i aria-hidden='true' className='fas fa-code-branch' />
-                            {project.data.forks_count}
-                        </p>
-                    </a>
-                </div>
-
             </div>
         );
     }
