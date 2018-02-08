867e193c2bae45adf37d5d37375eb7dcd226177b
ldevernay <boblehamster@yahoo.fr>
2018-02-08 09:58:08 +0100

    clean code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,17 @@
 import axios from 'axios';
 
 export const GET_PROJECT = 'GET_PROJECT';
-export const SELECT_PROJECT = 'SELECT_PROJECT';
 
 export function getProject(project) {
     const URL = `https://api.github.com/repos/${project.repo}`;
+    axios.create({
+        baseURL: URL,
+        headers: {'Accept': 'application/vnd.github.full+json'}
+    })
     const request = axios.get(URL);
 
     return {
         type: GET_PROJECT,
         payload: request
     };
-}
-
-export function selectProject(project) {
-    return {
-        type: SELECT_PROJECT,
-        payload: project
-    }
 }
